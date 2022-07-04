66e8c41737e6ec5f604eab4f198dd030ffc262cb
Shreedhan003 <shridhan98@gmail.com>
2022-07-04 16:25:59 +0530

    lavender: Initial build properties

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,4 +14,10 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vendor.qti.gnss@3.0-service.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 -include vendor/xiaomi/lavender/BoardConfigVendor.mk
